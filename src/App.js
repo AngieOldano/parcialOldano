33ed2755227b674a866672e8e1686bd9a55d20ae
root <angiedenise.oldano@estudiantes.unahur.edu.ar>
2023-06-26 19:29:04 -0300

    probando div responsive 3

--- src/App.js
+++ src/App.js
@@ -30,10 +30,10 @@ function App() {
 
           <Container className='m-5'>
             <Row>
-              <Col xs={9} md={6}>
+              <Col xs={12} md={6}>
                 <img className="img-fluid" width={500} height={300} src="https://i.pinimg.com/originals/21/07/e2/2107e20536b74138c457f503a975162b.jpg" alt="Imagen" />
               </Col>
-              <Col xs={9} md={6} className="d-flex align-items-center">
+              <Col xs={12} md={6} className="d-flex align-items-center">
                 <div>
                   <h2>Título del texto</h2>
                   <h3>Contenido del texto...</h3>
